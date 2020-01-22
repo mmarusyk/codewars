2fd5d2a3e030920a3d41c36fc5159b0af58a807d
Mick <misha.marusyk@gmail.com>
2020-01-22 17:52:21 +0200

    Solve digitize

---
+++ Ruby/8kyu/digitize.rb
@@ -0,0 +1,20 @@
+=begin
+
+Convert number to reversed array of digits
+
+Given a random number:
+
+    C#: long;
+    C++: unsigned long;
+
+You have to return the digits of this number within an array in reverse order.
+Example:
+
+348597 => [7,9,5,8,4,3]
+
+=end
+
+#My Solution
+def digitize(n)
+  n.digits
+end
