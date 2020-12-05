221fe648bb9d262bcff587c0b0b27ddc246a1344
Mykhailo Marusyk <misha.marusyk@gmail.com>
2020-12-05 16:46:26 +0200

    Solve ruby/8kyu/Opposite number

---
+++ Ruby/8kyu/opposite.rb
@@ -0,0 +1,22 @@
+=begin
+
+Very simple, given a number, find its opposite.
+
+Examples:
+
+1: -1
+14: -14
+-34: 34
+
+=end
+
+# My solution
+def opposite(number)
+  -1 * number
+end
+
+# Clever solution
+
+def opposite n
+  -n
+end
